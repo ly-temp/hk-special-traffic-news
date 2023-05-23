a0ddc2fe1e9bd264c4cf95e8c41727c9d84d5690
ly-temp <100673133+ly-temp@users.noreply.github.com>
2023-05-23 01:30:53 +0000

    test

---
+++ main.py
@@ -0,0 +1,5 @@
+import os
+
+url = 'https://api.data.gov.hk/v1/historical-archive/get-file?url=https%3A%2F%2Fwww.td.gov.hk%2Ftc%2Fspecial_news%2Ftrafficnews.xml&time='
+
+os.makedirs('./json', exist_ok=True)
